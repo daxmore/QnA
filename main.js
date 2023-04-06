e77e7da4422048be398ccd6efc77ae8076fce8c0
daxmore <117888686+daxmore@users.noreply.github.com>
2023-04-06 23:59:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+function result() {
+    var score = 0;
+    if (document.getElementById('correct1').checked) {
+        score++;
+    }
+    if (document.getElementById('correct2').checked) {
+        score++;
+    }
+    if (document.getElementById('correct3').checked) {
+        score++;
+    }
+
+    if (document.getElementById('correct4').checked) {
+        score++;
+    }
+    if (document.getElementById('correct5').checked) {
+        score++;
+    }
+    if (document.getElementById('correct6').checked) {
+        score++;
+    }
+
+    if (document.getElementById('correct7').checked) {
+        score++;
+    }
+    if (document.getElementById('correct8').checked) {
+        score++;
+    }
+    if (document.getElementById('correct9').checked) {
+        score++;
+    }
+    if (document.getElementById('correct10').checked) {
+        score++;
+    }
+    if (document.getElementById('correct11').checked) {
+        score++;
+    }
+    if (document.getElementById('correct12').checked) {
+        score++;
+    }
+    if (document.getElementById('correct13').checked) {
+        score++;
+    }
+    if (document.getElementById('correct14').checked) {
+        score++;
+    }
+    if (document.getElementById('correct15').checked) {
+        score++;
+    }
+    alert("Your Score Is : " + score + "/15");
+}
